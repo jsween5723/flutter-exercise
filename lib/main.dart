660eef419884add347b6e8ba5ce939829c58869f
jsween5723 <jsween5723@gmail.com>
2023-12-23 07:17:24 +0900

    feat(스플래쉬): 배경색 바꾸기

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,13 @@ void main() {
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
+        body: Container(
+            decoration: const BoxDecoration(color: Colors.orange),
+            child: const Center(
           child: Text('Splash Screen'),
-        ),
+        ))
       ),
     );
   }
